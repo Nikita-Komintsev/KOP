863e640eb2347aba3e16f587a3514bc2b50c4886
nikita <nikita.comin@gmail.com>
2023-12-29 10:17:10 +0300

    loss_percentage in receiver_app.py

--- receiver_app.py
+++ receiver_app.py
@@ -1,8 +1,8 @@
 # receiver_app.py
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
 from PyQt5.QtNetwork import QTcpServer, QTcpSocket, QHostAddress
-
+import random
 
 class ReceiverApp(QWidget):
     def __init__(self):
@@ -13,12 +13,21 @@ class ReceiverApp(QWidget):
     def initUI(self):
         self.message_label = QLabel('Полученные сообщения:')
         self.message_display = QLabel()
+        self.loss_percentage_label = QLabel('Процент потерь пакетов:')
+        self.loss_percentage_edit = QLineEdit()
+        self.set_loss_percentage_button = QPushButton('Установить процент потерь')
+        self.loss_percentage = 0
 
         layout = QVBoxLayout()
         layout.addWidget(self.message_label)
         layout.addWidget(self.message_display)
+        layout.addWidget(self.loss_percentage_label)
+        layout.addWidget(self.loss_percentage_edit)
+        layout.addWidget(self.set_loss_percentage_button)
 
         self.setLayout(layout)
+
+        self.set_loss_percentage_button.clicked.connect(self.set_loss_percentage)
 
         self.server = QTcpServer(self)
         self.server.listen(QHostAddress('127.0.0.1'), 12345)  # Указать IP-адрес и порт
@@ -31,7 +40,21 @@ class ReceiverApp(QWidget):
     def receive_data(self):
         client_socket = self.sender()
         data = client_socket.readAll().data().decode()
-        self.message_display.setText(data)
+
+        if random.randint(1, 100) > self.loss_percentage:  # Моделирование потерь пакетов
+            current_text = self.message_display.text()
+            self.message_display.setText(f'{current_text}\n{data}')
+
+    def set_loss_percentage(self):
+        try:
+            self.loss_percentage = int(self.loss_percentage_edit.text())
+            self.loss_percentage_label.setText(f'Процент потерь пакетов: {self.loss_percentage}%')
+        except ValueError:
+            self.loss_percentage_label.setText('Введите корректное значение')
+
+    def closeEvent(self, event):
+        # Метод, вызываемый при закрытии окна
+        self.server.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- sender_app.py
+++ sender_app.py
@@ -1,8 +1,9 @@
 # sender_app.py
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QInputDialog
-from PyQt5.QtNetwork import QTcpSocket
 import random
+import string
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtNetwork import QTcpSocket
 
 class SenderApp(QWidget):
     def __init__(self):
@@ -14,18 +15,20 @@ class SenderApp(QWidget):
         self.message_label = QLabel('Введите сообщение:')
         self.message_edit = QLineEdit()
         self.send_button = QPushButton('Передать')
+        self.generate_button = QPushButton('Генерировать случайное сообщение')
         self.sequence_button = QPushButton('Запрос последовательности')
-        self.loss_percentage, _ = QInputDialog.getInt(self, 'Настройки', 'Процент потерь пакетов:', 0, 0, 100)
 
         layout = QVBoxLayout()
         layout.addWidget(self.message_label)
         layout.addWidget(self.message_edit)
         layout.addWidget(self.send_button)
+        layout.addWidget(self.generate_button)
         layout.addWidget(self.sequence_button)
 
         self.setLayout(layout)
 
         self.send_button.clicked.connect(self.send_message)
+        self.generate_button.clicked.connect(self.generate_random_message)
         self.sequence_button.clicked.connect(self.request_sequence)
 
         self.socket = QTcpSocket(self)
@@ -35,12 +38,15 @@ class SenderApp(QWidget):
 
     def send_message(self):
         message = self.message_edit.text()
-        if random.randint(1, 100) > self.loss_percentage:  # Моделирование потерь пакетов
-            self.socket.write(message.encode())
+        self.socket.write(message.encode())
+
+    def generate_random_message(self):
+        message = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        self.message_edit.setText(message)
 
     def request_sequence(self):
-        pass
         # Отправить запрос на последовательность
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
